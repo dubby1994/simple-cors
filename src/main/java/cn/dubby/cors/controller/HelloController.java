5705eb8135ab4455f61986b6c154f82109c89552
dubby <yangzheng03@meituan.com>
2017-12-01 16:58:30 +0800

    remove usued import

--- src/main/java/cn/dubby/cors/controller/HelloController.java
+++ src/main/java/cn/dubby/cors/controller/HelloController.java
@@ -1,7 +1,6 @@
 package cn.dubby.cors.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
