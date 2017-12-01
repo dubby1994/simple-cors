feeba846e4fdb5e5769c66484dedde8ffe4e83fb
dubby <yangzheng03@meituan.com>
2017-12-01 16:55:30 +0800

    init

---
+++ src/main/java/cn/dubby/cors/Application.java
@@ -0,0 +1,16 @@
+package cn.dubby.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by teeyoung on 17/12/1.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/cn/dubby/cors/controller/HelloController.java
@@ -0,0 +1,32 @@
+package cn.dubby.cors.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by teeyoung on 17/12/1.
+ */
+@RestController
+@RequestMapping(value = "hello")
+public class HelloController {
+
+    @RequestMapping(value = "simpleXSInvocation")
+    public String simpleXSInvocation(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        //下面这个即便有，也不能接受带有cookie的跨域请求，因为上面使用了 *
+        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
+        return "simpleXSInvocation";
+    }
+
+    @RequestMapping(value = "simpleXSInvocation2")
+    public String simpleXSInvocation2(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+        //有下面的header，就可以接受带cookie的跨域请求了
+        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
+        return "simpleXSInvocation2";
+    }
+}
