e39d12e2ab5ee8c94bfbde6cd402af93003abea9
dubby <yangzheng03@meituan.com>
2017-12-01 17:13:04 +0800

    use spring

--- src/main/java/cn/dubby/cors/Application.java
+++ src/main/java/cn/dubby/cors/Application.java
@@ -2,6 +2,10 @@ package cn.dubby.cors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by teeyoung on 17/12/1.
@@ -13,4 +17,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/hello/preflightInvocation2").allowedOrigins("http://localhost:9090");
+            }
+        };
+    }
 }
--- src/main/java/cn/dubby/cors/controller/HelloController.java
+++ src/main/java/cn/dubby/cors/controller/HelloController.java
@@ -1,5 +1,6 @@
 package cn.dubby.cors.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,11 +22,34 @@ public class HelloController {
         return "simpleXSInvocation";
     }
 
-    @RequestMapping(value = "simpleXSInvocation2")
-    public String simpleXSInvocation2(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+//    @CrossOrigin(origins = "http://localhost:9090")
+//    @RequestMapping(value = "simpleXSInvocation")
+//    public String simpleXSInvocation(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+//        return "simpleXSInvocation";
+//    }
+
+    @RequestMapping(value = "simpleXSInvocationWithCookie")
+    public String simpleXSInvocationWithCookie(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         httpServletResponse.addHeader("Access-Control-Allow-Origin", httpServletRequest.getHeader("Origin"));
         //有下面的header，就可以接受带cookie的跨域请求了
         httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
-        return "simpleXSInvocation2";
+        return "simpleXSInvocationWithCookie";
+    }
+
+//    @CrossOrigin(origins = "http://localhost:9090")
+//    @RequestMapping(value = "simpleXSInvocationWithCookie")
+//    public String simpleXSInvocationWithCookie(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+//        return "simpleXSInvocationWithCookie";
+//    }
+
+    @CrossOrigin(origins = "http://localhost:9090")
+    @RequestMapping(value = "preflightInvocation")
+    public String preflightInvocation(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        return "preflightInvocation";
+    }
+
+    @RequestMapping(value = "preflightInvocation2")
+    public String preflightInvocation2(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        return "preflightInvocation";
     }
 }
